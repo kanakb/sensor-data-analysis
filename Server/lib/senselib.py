ca9cc9078b52d012c31960247540ece418c6b907
kanakb@gmail.com <kanakb@gmail.com@1627692a-01a1-f231-e942-478d199961fa>
2011-04-08 05:38:31 +0000

    Server: database write support added.

--- Server/lib/senselib.py
+++ Server/lib/senselib.py
@@ -5,16 +5,15 @@ import time
 
 # Obtain a salted hash of some data
 def toHash(s):
+    # todo: throw the salt into the datastore
     salt = 'xmGskdd2gv3UZHpxOzq17FvndKPlr'
     hash = hashlib.md5( salt + str(s) ).hexdigest()
     return str(hash)
     
 # Convert unix time to datetime
-def toDateTime(unixTime):
-    #todo: implement this
-    return ""
+def toDateTime(t):
+    return datetime.datetime.fromtimestamp(float(t))
 
 # Convert datetime to unix time
-def toUnixTime(dateTime):
-    #todo: implement this
-    return ""
+def toUnixTime(t):
+    return time.mktime(t.timetuple()) + (1e-6) * t.microsecond
