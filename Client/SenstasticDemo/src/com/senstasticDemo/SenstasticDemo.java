3ec6c9ebf4cd042c2047a95799782101ffcd1580
maxvujovic@gmail.com <maxvujovic@gmail.com@1627692a-01a1-f231-e942-478d199961fa>
2011-04-15 02:06:03 +0000

    Senstastic.schedule(...) working.

--- Client/SenstasticDemo/src/com/senstasticDemo/SenstasticDemo.java
+++ Client/SenstasticDemo/src/com/senstasticDemo/SenstasticDemo.java
@@ -1,14 +1,8 @@
 package com.senstasticDemo;
 
-import java.util.Calendar;
-
-import com.senstastic.AlarmReceiver;
-
+import com.senstastic.Senstastic;
 
 import android.app.Activity;
-import android.app.AlarmManager;
-import android.app.PendingIntent;
-import android.content.Intent;
 import android.os.Bundle;
 
 public class SenstasticDemo extends Activity
@@ -18,18 +12,6 @@ public class SenstasticDemo extends Activity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
-        // Set the interval.
-        int interval = 20;
-        
-        // Create the Intent.
-        Intent intent = new Intent(this, AlarmReceiver.class);
-        intent.putExtra("sensorServiceClassName", SensorService.class.getName());
-        
-        // Create the PendingIntent.
-        PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
-
-        // Create the alarm.
-        AlarmManager alarmManager = (AlarmManager)getSystemService(ALARM_SERVICE);
-        alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, Calendar.getInstance().getTimeInMillis(), interval*1000, pendingIntent);
+        Senstastic.schedule(this, SensorService.class, 20);
     }
 }
