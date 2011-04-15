062e9cfcb2743b92e2586de88cc4f87db18478d8
maxvujovic@gmail.com <maxvujovic@gmail.com@1627692a-01a1-f231-e942-478d199961fa>
2011-04-15 01:59:38 +0000

    SensorService class name passed around in Intent extras and working.

--- Client/SenstasticDemo/src/com/senstastic/AlarmReceiver.java
+++ Client/SenstasticDemo/src/com/senstastic/AlarmReceiver.java
@@ -7,11 +7,23 @@ import com.senstasticDemo.SensorService;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
 
 public class AlarmReceiver extends BroadcastReceiver
 {
 	public void onReceive(Context context, Intent intent) 
 	{
-		WakefulIntentService.sendWakefulWork(context, SensorService.class);
+        try
+        {
+        	Bundle extras = intent.getExtras();
+        	String sensorServiceClassName = extras.getString("sensorServiceClassName");
+        	Class<?> sensorServiceClass = Class.forName(sensorServiceClassName);
+	            
+        	WakefulIntentService.sendWakefulWork(context, sensorServiceClass);
+        }
+        catch(ClassNotFoundException e)
+        {
+            System.err.println("Class not found!");
+        }
 	}
 }
