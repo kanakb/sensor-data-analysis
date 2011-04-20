e97d250852761c42f946c9e5156becb9e92106c6
maxvujovic@gmail.com <maxvujovic@gmail.com@1627692a-01a1-f231-e942-478d199961fa>
2011-04-20 23:41:58 +0000

    Experimenting with different location providers.

--- trunk/Client/SenstasticDemo/src/com/senstasticDemo/MySensorService.java
+++ trunk/Client/SenstasticDemo/src/com/senstasticDemo/MySensorService.java
@@ -1,19 +1,24 @@
 package com.senstasticDemo;
 
+import com.senstastic.LocationRequest;
 import com.senstastic.Measurement;
 import com.senstastic.SensorService;
 
 public class MySensorService extends SensorService
 {
+	LocationRequest locationRequest;
+	
 	protected int getInterval()
 	{
-		return 10;
+		return 300;
 	}
 	
 	protected void sense()
 	{
+		locationRequest = new LocationRequest(this, 100, 100000);
+		
 		Measurement.generate(this, "MySensor", new Integer(123));
 		
-		finishSensing();
+		finish();
 	}
 }
