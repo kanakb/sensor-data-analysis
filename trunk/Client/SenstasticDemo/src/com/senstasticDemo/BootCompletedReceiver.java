9798ce4af49dfee2bb25963b2e50e224c0c78a1f
maxvujovic@gmail.com <maxvujovic@gmail.com@1627692a-01a1-f231-e942-478d199961fa>
2011-04-25 20:43:33 +0000

    Added start service on boot code.

---
+++ trunk/Client/SenstasticDemo/src/com/senstasticDemo/BootCompletedReceiver.java
@@ -0,0 +1,13 @@
+package com.senstasticDemo;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class BootCompletedReceiver extends BroadcastReceiver 
+{
+	public void onReceive(Context context, Intent intent)
+	{
+		 
+	}
+}
