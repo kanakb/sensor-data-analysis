6a837bc80852f53aa4b9bb6c0e75acc569615fab
maxvujovic@gmail.com <maxvujovic@gmail.com@1627692a-01a1-f231-e942-478d199961fa>
2011-05-04 06:39:26 +0000

    Taking audio samples and getting the max amplitude as a 16-bit short.

--- trunk/Client/SenstasticDemo/src/com/senstasticDemo/MySensorService.java
+++ trunk/Client/SenstasticDemo/src/com/senstasticDemo/MySensorService.java
@@ -1,5 +1,10 @@
 package com.senstasticDemo;
 
+import android.media.AudioFormat;
+import android.media.AudioRecord;
+import android.media.MediaRecorder;
+
+import com.senstastic.Logger;
 import com.senstastic.SensorService;
 
 public class MySensorService extends SensorService
@@ -13,12 +18,63 @@ public class MySensorService extends SensorService
 	
 	protected int getInterval()
 	{
-		return 60;
+		return 15;
 	}
 	
 	protected void sense()
 	{
-		double data = 123.456;
-		finishSensing(data);
+		try
+		{
+			// Set the recording length.
+			double recordLengthInSecs = 5.0;
+			
+			// Initialize the audio recorder.
+			int sampleRateInHz = 44100;
+	        int sampleLengthInShorts = AudioRecord.getMinBufferSize(sampleRateInHz, AudioFormat.CHANNEL_CONFIGURATION_MONO, AudioFormat.ENCODING_PCM_16BIT);
+	        AudioRecord audioRecorder = new AudioRecord(MediaRecorder.AudioSource.MIC, sampleRateInHz, AudioFormat.CHANNEL_CONFIGURATION_MONO, AudioFormat.ENCODING_PCM_16BIT, sampleLengthInShorts);
+	        
+	        // Determine the number of samples to take. Each sample fills a buffer of length sampleLength.
+	        double sampleLengthInSec = (double)sampleLengthInShorts / (double)sampleRateInHz;
+	        int numSamples = (int)Math.ceil(recordLengthInSecs / sampleLengthInSec);
+	        
+            // Start recording.
+	        audioRecorder.startRecording();
+	        
+	        // Take audio samples and keep track of the maximum short value throughout all of the sample.
+	        short max = Short.MIN_VALUE;
+	        for (int s = 0; s < numSamples; s++)
+	        {
+		        // Read some audio data.
+		        short[] sampleData = new short[sampleLengthInShorts];
+		        int numShortsRead = audioRecorder.read(sampleData, 0, sampleLengthInShorts);
+	        	
+		        if (numShortsRead < 0)
+		        	throw new Exception("Could not take an audio sample!");
+		        
+		        // Update the maximum.
+		        for (int i = 0; i < numShortsRead; i++)
+		        {
+		        	if (sampleData[i] > max)
+		        		max = sampleData[i];
+		        }
+		        	
+		        ////Thread.sleep(sampleLengthInSec);
+	        }
+	        
+	        // Stop recording.
+	        audioRecorder.stop();
+	        
+	        Logger.d("max: " + max);
+	        
+	        // Finish sensing.
+			finishSensing(max);
+		}
+		catch(Exception e)
+		{
+			// TODO: Finish sensing.
+			
+			Logger.e(e.getMessage());
+		}
+
 	}
 }
