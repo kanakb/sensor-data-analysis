eacf2c9bec73bb0707b662db82d738e58836c65a
maxvujovic@gmail.com <maxvujovic@gmail.com@1627692a-01a1-f231-e942-478d199961fa>
2011-05-16 12:43:00 +0000

    Webview showing heatgrid. Need to fix centering issue.

--- Client/SenstasticDemo/src/com/senstasticDemo/MapActivity.java
+++ Client/SenstasticDemo/src/com/senstasticDemo/MapActivity.java
@@ -18,7 +18,7 @@ public class MapActivity extends Activity
 
         WebView webview = (WebView)findViewById(R.id.webview);
         webview.getSettings().setJavaScriptEnabled(true);
-        webview.loadUrl("http://maps.google.com");
+        webview.loadUrl("http://dudeitworks.com/heatgrid/heatgrid.html?dataUrl=data.xml&zoom=15&colors=00FF00,FFCC00,FF0000");
         
         Button next = (Button) findViewById(R.id.Button02);
         next.setOnClickListener(new View.OnClickListener() 
