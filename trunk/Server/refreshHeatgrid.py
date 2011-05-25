e860f8dde525570387ba734706c0d79ce6ec5672
kanakb@gmail.com <kanakb@gmail.com@1627692a-01a1-f231-e942-478d199961fa>
2011-05-25 07:54:59 +0000

    Adding personal heatgrid support.

--- trunk/Server/refreshHeatgrid.py
+++ trunk/Server/refreshHeatgrid.py
@@ -7,31 +7,36 @@ import urllib
 class HeatgridRefresher(webapp.RequestHandler):
     # Generate a new heatgrid (intended to be a cron job)
     def get(self):
-        try:
-            form_fields = {
-                "xml": '''
-                <measurements>
-                    <measurement>
-                        <minLatitude>34.0389</minLatitude>
-                        <maxLatitude>34.0761</maxLatitude>
-                        <minLongitude>-118.4838</minLongitude>
-                        <maxLongitude>-118.3901</maxLongitude>
-                        <sensorKinds>
-                            <sensorKind>volume</sensorKind>
-                        </sensorKinds>
-                    </measurement>
-                </measurements>
-                ''',
-                "xDim": "20",
-                "yDim": "20"
-            }
-            form_data = urllib.urlencode(form_fields)
-            result = urlfetch.fetch(url='http://sensor-analysis.appspot.com/heatgridGen',
-                        payload=form_data,
-                        method=urlfetch.POST,
-                        headers={'Content-Type': 'application/x-www-form-urlencoded'})
-        except:
-            pass
+        deviceIdData = ''
+        if self.request.get('id'):
+            deviceIdData = '''
+                    <deviceIds>
+                        <deviceId>''' + self.request.get('id') + '''</deviceId>
+                    </deviceIds>'''
+        xmlreq = '''
+            <measurements>
+                <measurement>''' + deviceIdData + '''
+                    <minLatitude>34.0389</minLatitude>
+                    <maxLatitude>34.0761</maxLatitude>
+                    <minLongitude>-118.4838</minLongitude>
+                    <maxLongitude>-118.3901</maxLongitude>
+                    <sensorKinds>
+                        <sensorKind>volume</sensorKind>
+                    </sensorKinds>
+                </measurement>
+            </measurements>
+            '''
+        form_fields = {
+            "xDim": "20",
+            "yDim": "20"
+        }
+        form_fields['xml'] = xmlreq
+        form_data = urllib.urlencode(form_fields)
+        result = urlfetch.fetch(url='http://sensor-analysis.appspot.com/heatgridGen',
+                    payload=form_data,
+                    method=urlfetch.POST,
+                    headers={'Content-Type': 'application/x-www-form-urlencoded'})
+        
 
 application = webapp.WSGIApplication(
                                      [('/refreshHeatgrid', HeatgridRefresher)],
