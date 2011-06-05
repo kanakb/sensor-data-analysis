673deafea1b1c9a967719400df49d0670383a4ca
maxvujovic@gmail.com <maxvujovic@gmail.com@1627692a-01a1-f231-e942-478d199961fa>
2011-06-05 22:08:31 +0000

    Fixed BootCompletedReceiver not being called bug. Added debug Toast message.

--- trunk/Client/SenstasticDemo/src/com/senstasticDemo/BootCompletedReceiver.java
+++ trunk/Client/SenstasticDemo/src/com/senstasticDemo/BootCompletedReceiver.java
@@ -3,11 +3,17 @@ package com.senstasticDemo;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.widget.Toast;
 
 public class BootCompletedReceiver extends BroadcastReceiver 
 {
 	public void onReceive(Context context, Intent intent)
 	{
+		// For debugging purposes, show a message that Senstastic is initializing when the phone boots up.
+		Toast toast = Toast.makeText(context, "Senstastic Initializing...", 10);
+		toast.show();
+		
+		// Initialize Senstastic.
 		SenstasticDemo.initSenstastic(context);
 	}
 }
